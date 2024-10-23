3b1cd9c685f1eba8b4e673a5ccb59c1cf75eadd8
bcalou <bastien.calou@gmail.com>
2024-10-23 19:08:48 +0200

    merge todo-store and store

--- src/js/store/todo-store.js
+++ src/js/store/todo-store.js
@@ -1,20 +1,129 @@
-import { Store } from "./store.js";
-
 /**
- * Store for the todos
+ * IndexedDB + WS todo store
  */
-class TodoStore extends Store {
+class TodoStore {
   constructor() {
-    super("todo-list", "todo");
+    this.dbName = "todo-list";
+    this.storeName = "todo";
+
+    this.listeners = [];
+    this.itemsListeners = new Map();
+
+    this.ready = this.initializeDB();
   }
 
-  add(label) {
-    const todo = {
-      label,
-      done: 0, // Use 0 instead of false to prepare the use of sqlite
-    };
+  // Register a function that will be called when the database is updated
+  // This way, components can keep track of what's happening
+  subscribe(callback) {
+    this.listeners.push(callback);
 
-    return super.add(todo);
+    // Return a method that can be called to unsubscribe
+    return () => this.unsubscribe(callback);
+  }
+
+  unsubscribe(callback) {
+    this.listeners = this.listeners.filter((listener) => listener !== callback);
+  }
+
+  // Notify the changes to each listeners
+  async notify() {
+    const items = new Map((await this.getAll()).map((item) => [item.id, item]));
+    this.listeners.forEach((callback) => callback(items));
+  }
+
+  async initializeDB() {
+    return await new Promise((resolve, reject) => {
+      const request = indexedDB.open(this.dbName);
+
+      request.onupgradeneeded = (event) => {
+        const db = event.target.result;
+        db.createObjectStore(this.storeName, {
+          keyPath: "id",
+          autoIncrement: true,
+        });
+      };
+
+      request.onsuccess = (event) => {
+        this.db = event.target.result;
+        resolve(this.db);
+      };
+
+      request.onerror = (event) => {
+        reject("Database error: " + event.target.errorCode);
+      };
+    });
+  }
+
+  // Base function to perform anything in the table, used by read & write
+  async request(getRequest, mode, onSuccess) {
+    await this.ready;
+
+    return await new Promise((resolve, reject) => {
+      const transaction = this.db.transaction(this.storeName, mode);
+      const store = transaction.objectStore(this.storeName);
+      const request = getRequest(store);
+
+      request.onsuccess = () => {
+        resolve(request.result);
+
+        if (onSuccess) {
+          onSuccess(request.result, store);
+        }
+      };
+
+      request.onerror = () => {
+        reject(`Error executing ${this.storeName} task: ${request.errorCode}`);
+      };
+    });
+  }
+
+  // Base function to read in the table
+  async read(getRequest) {
+    return this.request(getRequest, "readonly");
+  }
+
+  // Base function to write in the table
+  async write(getRequest, onSuccess, notify = true) {
+    return this.request(getRequest, "readwrite", (result, store) => {
+      if (notify) {
+        this.notify();
+      }
+
+      if (onSuccess) {
+        onSuccess(result, store);
+      }
+    });
+  }
+
+  // Base function to perform an operation on each item
+  async writeForEach(getRequest, onSuccess) {
+    return await this.write(
+      (store) => store.openCursor(),
+      (result, store) => {
+        const cursor = result;
+
+        if (cursor) {
+          const request = getRequest(cursor, store);
+
+          if (request && onSuccess) {
+            request.onsuccess = onSuccess(cursor);
+          }
+
+          cursor.continue();
+        } else {
+          this.notify();
+        }
+      },
+      false // Don't notify for each element
+    );
+  }
+
+  async getAll() {
+    return await this.read((store) => store.getAll());
+  }
+
+  async getById(id) {
+    return await this.read((store) => store.get(id));
   }
 
   // Get an object containing the total, done and remaining todos count
@@ -31,6 +140,33 @@ class TodoStore extends Store {
     return count;
   }
 
+  async add(label) {
+    const todo = {
+      label,
+      done: 0,
+    };
+
+    await this.write(
+      (store) => store.add(todo),
+      (result) => console.info(`Added ${this.storeName} #${result}`)
+    );
+  }
+
+  async update(id, changes) {
+    const item = await this.getById(id);
+
+    if (item) {
+      for (const [key, value] of Object.entries(changes)) {
+        item[key] = value;
+      }
+
+      return this.write(
+        (store) => store.put(item),
+        () => console.info(`Updated ${this.storeName} #${id}`)
+      );
+    }
+  }
+
   async markAllAsDone() {
     return await this.writeForEach(
       (cursor, store) =>
@@ -42,6 +178,13 @@ class TodoStore extends Store {
             }),
       (cursor) =>
         console.info(`Set todo #${cursor.value.id} done property to true`)
+    );
+  }
+
+  async delete(id) {
+    return this.write(
+      (store) => store.delete(id),
+      () => console.info(`Deleted ${this.storeName} #${id}`)
     );
   }
 
